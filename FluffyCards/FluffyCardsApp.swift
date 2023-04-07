edf3dcdd31409deeb05525b95939012d95178e15
Alexander Snitko <asyncamisu@gmail.com>
2023-04-07 20:22:40 +0300

    Initial Commit

---
+++ FluffyCards/FluffyCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FluffyCardsApp.swift
+//  FluffyCards
+//
+//  Created by Alexander Snitko on 7.04.23.
+//
+
+import SwiftUI
+
+@main
+struct FluffyCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
